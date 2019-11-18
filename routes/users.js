15a164dde3eb48c1c90f63b9641eaa1ed445493e
Silifonov Alexey <silifonov@gmail.com>
2019-11-18 15:31:56 +0200

    add auth token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/Users');
@@ -41,10 +43,28 @@ router.post(
             });
 
             const salt = await bcrypt.genSalt(10);
+
             user.password = await bcrypt.hash(password, salt);
 
             await user.save();
-            res.send('User Saved');
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {
+                    expiresIn: 3600000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (err) {
             console.log(err.message);
             res.send(500).send('Server Error');
